6f0fb6bc97c0c6e144dacc49584e74c510e2e891
DANIEL GONZALEZ <126337541+DanielGonzalez112601@users.noreply.github.com>
2023-06-18 20:31:34 -0300

    segunda pre-entrega

--- index.js
+++ index.js
@@ -1,108 +1,140 @@
-let edad = parseInt (prompt("Hola , Ingresa tu edad:"));
+
+
+
+let edad = parseInt(prompt("Hola , Ingresa tu edad:"));
 
 if (edad >= 18) {
-  alert("Genial, Puedes acceder al préstamo");
+    alert("Genial, Puedes acceder al préstamo");
 } else {
-  alert("Lamentablemente No puedes acceder al préstamo");
+    alert("Lamentablemente No puedes acceder al préstamo");
 }
+
 
 let suma = 0;
 
 for (let i = 1; i <= 10; i++) {
-  suma += i;
+    suma += i;
 }
 
 console.log(`La suma de los números del 1 al 10 es ${suma}`);
 
 
-let opcion = 0;
-while (opcion < 1 || opcion > 4) {
-  opcion = parseInt(prompt("Selecciona una opción:\n1. Préstamo personal\n2. Préstamo automotor\n3. Préstamo profesional\n4. Préstamo inmobiliario"));
+function solicitarPrestamo() {
+
+    let opcion = 0;
+    while (opcion < 1 || opcion > 4) {
+        opcion = parseInt(prompt("Selecciona una opción:\n1. Préstamo personal\n2. Préstamo automotor\n3. Préstamo profesional\n4. Préstamo inmobiliario"));
+    }
+
+    let nombreOpcion;
+    switch (opcion) {
+        case 1:
+            nombreOpcion = "Préstamo personal";
+            break;
+        case 2:
+            nombreOpcion = "Préstamo automotor";
+            break;
+        case 3:
+            nombreOpcion = "Préstamo profesional";
+            break;
+        case 4:
+            nombreOpcion = "Préstamo inmobiliario";
+            break;
+    }
+    alert(`Has seleccionado la opción ${opcion}: ${nombreOpcion}`);
+
+    let nombre = prompt("Ingresa tu nombre:");
+    let cantidad = parseFloat(prompt("Ingresa la cantidad deseada:"));
+    let plazo = parseInt(prompt("Ingresa el plazo en meses:"));
+
+    let solicitud = {
+        nombre: nombre,
+        cantidad: cantidad,
+        plazo: plazo
+    };
+
+    procesarSolicitud(solicitud);
+
+    alert("Su préstamo ha sido aprobado. ¡Gracias por su preferencia!");
 }
+let tasaInteres = 0.1; 
+let historialSolicitudes = []; 
 
+// Funciones
+function procesarSolicitud(solicitud) {
+    let intereses = solicitud.cantidad * tasaInteres * solicitud.plazo;
+    let totalPagar = solicitud.cantidad + intereses;
 
-let nombreOpcion;
-switch (opcion) {
-  case 1:
-    nombreOpcion = "Préstamo personal";
-    break;
-  case 2:
-    nombreOpcion = "Préstamo automotor";
-    break;
-  case 3:
-    nombreOpcion = "Préstamo profesional";
-    break;
-  case 4:
-    nombreOpcion = "Préstamo inmobiliario";
-    break;
+    let resultado = {
+        nombre: solicitud.nombre,
+        cantidad: solicitud.cantidad,
+        plazo: solicitud.plazo,
+        intereses: intereses,
+        totalPagar: totalPagar
+    };
+
+    historialSolicitudes.push(resultado);
+    mostrarResultado(resultado);
 }
-alert(`Has seleccionado la opción ${opcion}: ${nombreOpcion}`);
-
-
-let monto = parseInt(prompt("Ingresa el monto del préstamo:"));
-
-let cuotas = parseInt(prompt("Ingresa la cantidad de cuotas:"));
-
-
-let tasaIVA = 0.22;
-let valorCuotaSinIVA = monto / cuotas;
-let valorCuotaConIVA = valorCuotaSinIVA * (1 + tasaIVA);
-
-
-alert(`El valor de cada cuota es de ${valorCuotaConIVA.toFixed(2)} (IVA incluido)`);
-
-alert("Su préstamo ha sido aprobado. ¡Gracias por su preferencia!");
+function mostrarResultado(resultado) {
+    alert("Nombre: " + resultado.nombre +
+        "\nCantidad solicitada: $" + resultado.cantidad.toFixed(2) +
+        "\nPlazo: " + resultado.plazo + " meses" +
+        "\nIntereses: $" + resultado.intereses.toFixed(2) +
+        "\nTotal a pagar: $" + resultado.totalPagar.toFixed(2));
+}
 
 class Persona {
     constructor(nombre, apellido, edad) {
-      this.nombre = nombre;
-      this.apellido = apellido;
-      this.edad = edad;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
     }
-  
-    saludar() {
-      console.log(`Hola, mi nombre es ${this.nombre} ${this.apellido} y tengo ${this.edad} años.`);
-    }
-  }
 
-  const persona = new Persona("Daniel", "González", 26);
+    saludar() {
+        console.log(`Hola, mi nombre es ${this.nombre} ${this.apellido} y tengo ${this.edad} años.`);
+    }
+}
+
+const persona = new Persona("Daniel", "González", 26);
+const persona2 = new Persona ("juan", "Perez", 40 );
 
 
 //   funcion
 
-  function sumar(a, b) {
+function sumar(a, b) {
     return a + b;
-  }
-  
-  let numero1 = 5;
-  let numero2 = 3;
-  let resultado = sumar(numero1, numero2);
-  console.log(resultado);
+}
+
+let numero1 = 5;
+let numero2 = 3;
+let resultado = sumar(numero1, numero2);
+console.log(resultado);
 
 //   array 
 
-  function sumarArray(array) {
+function sumarArray(array) {
     let suma = 0;
-    
+
     for (let i = 0; i < array.length; i++) {
-      suma += array[i];
+        suma += array[i];
     }
-    
+
     return suma;
-  }
-  
-  let numeros = [1, 2, 3, 4, 5];
-  let resultado2 = sumarArray(numeros);
-  console.log(resultado2);
+}
+
+let numeros = [1, 2, 3, 4, 5];
+let resultado2 = sumarArray(numeros);
+console.log(resultado2);
 
 // funcions de orden superior
 
 function ejecutarFuncion(funcion) {
     funcion();
-  }
-  
-  function saludar() {
-    console.log("¡Hola! Esta función fue ejecutada por otra función.");
-  }
+}
 
-  ejecutarFuncion(saludar);
+function saludar() {
+    console.log("¡Hola! Esta función fue ejecutada por otra función.");
+}
+
+ejecutarFuncion(saludar);
